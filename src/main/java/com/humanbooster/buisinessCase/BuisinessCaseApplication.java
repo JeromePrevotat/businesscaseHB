e3dc484f6c80004b3ddb9407d1dae8358579eec4
JeromePrevotat <jer.prevotat@gmail.com>
2025-05-27 11:36:02 +0200

    initial commit

---
+++ src/main/java/com/humanbooster/buisinessCase/BuisinessCaseApplication.java
@@ -0,0 +1,13 @@
+package com.humanbooster.buisinessCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuisinessCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuisinessCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/humanbooster/buisinessCase/BuisinessCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanbooster.buisinessCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuisinessCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
