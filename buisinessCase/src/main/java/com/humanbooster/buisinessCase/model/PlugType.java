c2006eee0b30c53e3c783f0ae8717b53345fc821
JeromePrevotat <jer.prevotat@gmail.com>
2025-05-27 13:53:32 +0200

    Class Plug

---
+++ buisinessCase/src/main/java/com/humanbooster/buisinessCase/model/PlugType.java
@@ -0,0 +1,29 @@
+package com.humanbooster.buisinessCase.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="plug_type")
+public class PlugType {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @NotBlank
+    @Column(name="plug_name")
+    private String plugName;
+
+}
