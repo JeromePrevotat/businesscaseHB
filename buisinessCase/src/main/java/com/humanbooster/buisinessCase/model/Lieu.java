65d697097188fe09500bf683bb1d19582723a5ab
JeromePrevotat <jer.prevotat@gmail.com>
2025-05-27 14:57:34 +0200

    Class Lieu

---
+++ buisinessCase/src/main/java/com/humanbooster/buisinessCase/model/Lieu.java
@@ -0,0 +1,26 @@
+package com.humanbooster.buisinessCase.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="location")
+public class Lieu {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="instruction")
+    private String instruction;
+}
