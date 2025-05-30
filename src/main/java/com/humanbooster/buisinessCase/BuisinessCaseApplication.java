a52f1a199d1a53c83de055f1e94fd7f41cff9008
JeromePrevotat <jer.prevotat@gmail.com>
2025-05-30 10:34:49 +0200

    stop spamming my mail

--- src/main/java/com/humanbooster/buisinessCase/BuisinessCaseApplication.java
+++ src/main/java/com/humanbooster/buisinessCase/BuisinessCaseApplication.java
@@ -1,13 +1,12 @@
 package com.humanbooster.buisinessCase;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BuisinessCaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BuisinessCaseApplication.class, args);
+		
 	}
 
 }
