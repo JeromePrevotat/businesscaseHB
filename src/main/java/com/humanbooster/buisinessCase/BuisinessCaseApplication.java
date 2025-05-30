434e8b6c18c79efe77c8c5acfc3f91393c6bf9dd
JeromePrevotat <jer.prevotat@gmail.com>
2025-05-30 11:26:55 +0200

    apirest + main run project

--- src/main/java/com/humanbooster/buisinessCase/BuisinessCaseApplication.java
+++ src/main/java/com/humanbooster/buisinessCase/BuisinessCaseApplication.java
@@ -1,12 +1,55 @@
 package com.humanbooster.buisinessCase;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.humanbooster.buisinessCase.apirest.ApiApplication;
 
 @SpringBootApplication
 public class BuisinessCaseApplication {
 
 	public static void main(String[] args) {
+	// STARTS & CONNECTS TO DB
+        SessionFactory sessionFactory = runDB();
+        
+        try {
+            runServlet();
+            SpringApplication.run(BuisinessCaseApplication.class, args);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static SessionFactory runDB(){
+        System.out.println("Starting App...\n");
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure()
+            .build();
+        Metadata metadata = new MetadataSources(registry).buildMetadata();
+        System.out.println("Connection Successfull !");
+        return metadata.buildSessionFactory();
+    }
+
+    private static void runServlet() throws Exception {
+		ResourceConfig config = new ApiApplication();
 		
-	}
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(server, "/");
+        context.addServlet(servlet, "/*");
+        server.start();
+        server.join();
+    }
 
 }
---
+++ src/main/java/com/humanbooster/buisinessCase/apirest/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.humanbooster.buisinessCase.apirest;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class ApiApplication extends ResourceConfig{
+    public ApiApplication(){
+        packages("com.humanbooster.buisinessCase",
+            "com.humanbooster.buisinessCase.apirest",
+            "com.humanbooster.buisinessCase.model",
+            "com.humanbooster.buisinessCase.repository",
+            "com.humanbooster.buisinessCase.service",
+            "com.humanbooster.buisinessCase.controller",
+            "com.humanbooster.buisinessCase.utils");
+        register(JacksonFeature.class);
+    }
+}
