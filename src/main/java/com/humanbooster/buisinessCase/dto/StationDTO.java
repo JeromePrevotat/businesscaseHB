bec50d286fc28fe34350c8f4e7ffc5b2f7457013
JeromePrevotat <jer.prevotat@gmail.com>
2025-05-31 18:42:22 +0200

    Station DTO  added from common base

--- src/main/java/com/humanbooster/buisinessCase/dto/StationDTO.java
+++ src/main/java/com/humanbooster/buisinessCase/dto/StationDTO.java
@@ -1,4 +1,4 @@
-package com.humanbooster.buisinessCase.model;
+package com.humanbooster.buisinessCase.dto;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -7,10 +7,13 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.humanbooster.buisinessCase.model.Rate;
+import com.humanbooster.buisinessCase.model.Reservation;
+import com.humanbooster.buisinessCase.model.Spot;
+import com.humanbooster.buisinessCase.model.StationState;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
@@ -22,7 +25,6 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
@@ -31,86 +33,50 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+/**
+ * Station's DTO
+ * Prevents circular references
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="stations")
-public class Station {
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id")
-    private long id;
+public class StationDTO {
+    private Long id;
     
     @NotBlank(message = "Station name cannot be blank")
-    @Column(name="station_name")
     private String stationName;
 
-    @Column(name = "latitude", precision = 10, scale = 8, nullable = false)
     @DecimalMin(value = "-90.0", message = "Latitude must be between -90 and 90")
     @DecimalMax(value = "90.0", message = "Latitude must be between -90 and 90")
     @NotNull(message = "Latitude is required")
     private BigDecimal latitude;
 
-    @Column(name = "longitude", precision = 11, scale = 8, nullable = false)
     @DecimalMin(value = "-180.0", message = "Longitude must be between -180 and 180")
     @DecimalMax(value = "180.0", message = "Longitude must be between -180 and 180")
     @NotNull(message = "Longitude is required")
     private BigDecimal longitude;
 
-    @Column(name="power_output", precision = 10, scale = 2, nullable = false)
     @DecimalMin(value = "0.1", message = "Power output must be positive")
     @NotNull(message = "Power output is required")
     private double powerOutput;
     
-    @Column(name="instruction", columnDefinition= "TEXT")
     private String instruction;
     
-    @Column(name="grounded", nullable = false)
     @NotNull(message = "Grounded status is required")
     private boolean grounded = true;
     
-    @Enumerated(EnumType.STRING)
-    @Column(name="state", length = 20, nullable = false)
     @NotNull(message = "Station state is required")
     private StationState state;
 
     @NotNull(message = "Busy status is required")
-    @Column(name="busy", nullable = false)
     private boolean busy =  false;
     
-    @NotNull(message = "Creation Date is required")
-    @Column(name = "creation_date", nullable = false)
-    private LocalDateTime creationDate = LocalDateTime.now();
-
-    @Column(name = "last_maintenance")
+    private LocalDateTime creationDate;
     private LocalDateTime lastMaintenance;
 
     @NotNull(message = "Wired status is required")
-    @Column(name="wired")
     private boolean wired = false;
 
-    @NotNull(message = "Spot is required")
-    @ManyToOne
-    @JoinColumn(name="id", nullable = false)
-    @JsonBackReference("station-spot")
-    private Spot spot;
 
-    @OneToMany(mappedBy = "station", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JsonManagedReference("station-rate")
-    private List<Rate> rateList;
-
-    @OneToMany(targetEntity=Reservation.class, mappedBy="id", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JsonManagedReference("station-reservation")
-    private List<Reservation> reservationList;
-    
-    @NotNull
-    @ManyToMany(targetEntity=Media.class, mappedBy="id", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinTable(
-        name = "media_station",
-        joinColumns = @JoinColumn(name = "station_id"),
-        inverseJoinColumns = @JoinColumn(name = "media_id")
-    )
-    @JsonIgnore
-    private List<Media> mediaList;
+    private SpotDTO spot;
 }
