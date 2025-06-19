128fbe1816a91392e9ae3f416745093b88e5e5f4
JeromePrevotat <jer.prevotat@gmail.com>
2025-06-19 12:40:26 +0200

    jwt auth done

--- src/main/java/com/humanbooster/buisinessCase/controller/AuthController.java
+++ src/main/java/com/humanbooster/buisinessCase/controller/AuthController.java
@@ -3,6 +3,9 @@ package com.humanbooster.buisinessCase.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.humanbooster.buisinessCase.service.JwtService;
+import com.humanbooster.buisinessCase.security.AuthRequestDTO;
+import com.humanbooster.buisinessCase.security.JwtDTO;
 
 import lombok.AllArgsConstructor;
 
@@ -27,36 +32,22 @@ public class AuthController {
     private final UserDetailsService userDetailsService;
 
     @PostMapping("/login")
-    public ResponseEntity<TokenDTO> authenticate(@RequestBody AuthRequest authRequest) {
+    public ResponseEntity<JwtDTO> authenticate(@RequestBody AuthRequestDTO authRequest) {
         // Authenticate the user with Spring Manager
         // CAN THROW BADCREDENTIALSEXCEPTION IF AUTHENTICATION FAILS
-        authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
-        );
+        try {
+            authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+            );
+        } catch (DisabledException | LockedException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
+        }
         // If authentication is successful, generate a JWT token
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
         final String jwtToken = jwtService.generateToken(userDetails.getUsername());
 
-        return ResponseEntity.status(HttpStatus.OK).body(new TokenDTO(jwtToken));
+        return ResponseEntity.status(HttpStatus.OK).body(new JwtDTO(jwtToken));
     }
-}
-
-// DTO for authentication request
-record AuthRequest(String username, String password) {
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-}
-
-record TokenDTO (String token) {
-
-    public String getToken() {
-        return token;
-    }
-    
 }
