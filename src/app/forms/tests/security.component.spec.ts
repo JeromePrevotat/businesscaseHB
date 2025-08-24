7934868bf9aed90e1890141a34c513b6fc8ad09c
JeromePrevotat <jer.prevotat@gmail.com>
2025-08-24 16:58:14 +0200

    moved tests where they be by default ffs

--- src/app/forms/tests/security.component.spec.ts
+++ src/app/forms/tests/security.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SecurityComponent } from './security.component';
+import { SecurityComponent } from '../security/security.component';
 
 describe('SecurityComponent', () => {
   let component: SecurityComponent;
