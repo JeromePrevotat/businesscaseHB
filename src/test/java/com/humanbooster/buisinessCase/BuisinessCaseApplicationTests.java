dcb1022ae66fe5dfd2aebf477739eee2592009e7
JeromePrevotat <jer.prevotat@gmail.com>
2025-08-26 14:13:28 +0200

    pipepline fix

--- src/test/java/com/humanbooster/buisinessCase/BuisinessCaseApplicationTests.java
+++ src/test/java/com/humanbooster/buisinessCase/BuisinessCaseApplicationTests.java
@@ -2,6 +2,7 @@ package com.humanbooster.buisinessCase;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class BuisinessCaseApplicationTests {
