0cea9c3f24fe23c40f93decf8d207f10ad138804
JeromePrevotat <jer.prevotat@gmail.com>
2025-10-27 12:22:14 +0100

    feat: User can create Station Created Station and StationState models in frontend

---
+++ frontend/src/app/models/station.ts
@@ -0,0 +1,18 @@
+import { StationState } from "./StationState";
+
+export interface Station {
+  readonly id: number | null;
+  stationName: string;
+	latitude: number; // -90 à 90
+	longitude: number; // -180 à 180
+	priceRate: number; // > 0.1
+	powerOutput: number; // > 0.1
+	manual?: string;
+	state: StationState;
+	grounded: boolean;
+	busy: boolean;
+	wired: boolean;
+	spot_id: number;
+	owner_id?: number;
+	plugTypeList: number[];
+}
