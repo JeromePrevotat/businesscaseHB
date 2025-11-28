d2aadf900d6be8f4c06cfe02e683e19471d0997a
JeromePrevotat <jer.prevotat@gmail.com>
2025-11-28 15:13:31 +0100

    trying to fix 403 again

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Environment pour production (build Docker)
 export const environment = {
   production: true,
-  apiUrl: 'http://91.166.39.12:8080'
+  apiUrl: 'http://electricity-business.91.166.39.12.nip.io'
 };
