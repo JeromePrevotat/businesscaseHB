b27c025cf22dc7ada8373993fb2a5c6609e5a3e0
JeromePrevotat <jer.prevotat@gmail.com>
2025-11-28 15:51:10 +0100

    fix: sql script

--- backend/scripts/populate.sql
+++ backend/scripts/populate.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE IF NOT EXISTS db;
 USE db;
 
--- Disable foreign key checks to allow truncation
--- and insertion without constraint violations
+-- Disable FK checks
 SET FOREIGN_KEY_CHECKS=0;
+
 TRUNCATE TABLE reservations;
 TRUNCATE TABLE stations;
 TRUNCATE TABLE spots;
@@ -15,23 +15,51 @@ TRUNCATE TABLE users;
 TRUNCATE TABLE roles_users;
 TRUNCATE TABLE roles;
 TRUNCATE TABLE jwt_refresh;
+
 SET FOREIGN_KEY_CHECKS=1;
 
+
+-- Insert roles
 INSERT INTO roles (id, role_name) VALUES
     (1, 'ADMIN'),
-    (2, 'USER'), 
+    (2, 'USER'),
     (3, 'GUEST');
 
-INSERT INTO users (id, username, firstname,
-                    lastname, password, email,
-                    birthdate, inscription_date, account_valid,
-                    validation_code, iban,
-                    banned)
-VALUES
-    (1, 'admin', 'Admin', 'Admin', '$2y$10$V74dRsCZ9dQSroSgVNZIIem5m6dYib.tMdYHmGUiWg1aeLnKAdXoC', 'admin@example.com', '1990-01-01', '2025-06-10 10:00:00', true, 'VAL123', 'FR7612345678901234567890623', false);
 
-INSERT INTO roles_users (user, role) VALUES
-    (1, 1), (1, 2),  -- admin as ADMIN & USER
+-- Insert admin user
+INSERT INTO users (
+    id, username, firstname, lastname,
+    password, email, birthdate,
+    inscription_date, account_valid,
+    validation_code, iban, banned
+) VALUES
+(
+    1,
+    'admin',
+    'Admin',
+    'Admin',
+    '$2y$10$V74dRsCZ9dQSroSgVNZIIem5m6dYib.tMdYHmGUiWg1aeLnKAdXoC',
+    'admin@example.com',
+    '1990-01-01',
+    '2025-06-10 10:00:00',
+    1,                -- true
+    'VAL123',
+    'FR7612345678901234567890623',
+    0                 -- false
+);
 
+
+-- Link admin to roles
+INSERT INTO roles_users (`user`, role) VALUES
+    (1, 1),
+    (1, 2); -- admin as ADMIN & USER
+
+
+-- Insert refresh token
 INSERT INTO jwt_refresh (id, refresh_token, user_id, issued_at) VALUES
-(1, 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTcxOTIzMDQwMCwiZXhwIjoxNzE5ODM1MjAwfQ.4iBq_sD-F8gZ7kY9xJ6vH5tW3cR2bA1fL0oE9jD4kGc', 1, '2025-06-24 12:00:00');
+(
+    1,
+    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTcxOTIzMDQwMCwiZXhwIjoxNzE5ODM1MjAwfQ.4iBq_sD-F8gZ7kY9xJ6vH5tW3cR2bA1fL0oE9jD4kGc',
+    1,
+    '2025-06-24 12:00:00'
+);
